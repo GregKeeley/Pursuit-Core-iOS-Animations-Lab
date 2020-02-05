116dd713535086fc5e8eeba247e6aa6e83cd4c5f
Gregory Keeley <gregorykeeley@pursuit.org>
2020-02-04 22:41:49 -0500

    - Created left and right buttons with functions - Created UISlider to control time of animation

--- Animations-Lab/Animations-Lab/ViewController.swift
+++ Animations-Lab/Animations-Lab/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var animationTime = 2.0 {
+        didSet {
+            timeLabel.text = String(format: "Time: %.2f", animationTime)
+        }
+    }
+    lazy var timeSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 0.1
+        slider.maximumValue = 10.0
+        slider.setValue(Float(animationTime), animated: true)
+        slider.addTarget(self, action: #selector(changeAnimationTime), for: .touchUpInside)
+        return slider
+    }()
+    
+    lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = label.font.withSize(30)
+        label.text = String(format: "Time: %.2f", animationTime)
+        return label
+    }()
+    
     lazy var blueSquare: UIView = {
         let view = UIView()
         view.backgroundColor = .blue
@@ -17,17 +39,32 @@ class ViewController: UIViewController {
     }()
     
     lazy var buttonStackView: UIStackView = {
-       let buttonStack = UIStackView()
+        let buttonStack = UIStackView()
         buttonStack.axis = .horizontal
-        buttonStack.alignment = .center
-        buttonStack.distribution = .equalSpacing
+        buttonStack.alignment = .fill
+        buttonStack.distribution = .fillProportionally
         buttonStack.spacing = 30
         return buttonStack
     }()
-    
+    lazy var leftButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("LEFT", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(animateSquareLeft(sender:)), for: .touchUpInside)
+        return button
+    }()
+    lazy var rightButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("RIGHT", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(animateSquareRight(sender:)), for: .touchUpInside)
+        return button
+    }()
     lazy var upButton: UIButton = {
-       let button = UIButton()
-        button.setTitle("Move square up", for: .normal)
+        let button = UIButton()
+        button.setTitle("UP", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = .cyan
         button.addTarget(self, action: #selector(animateSquareUp(sender:)), for: .touchUpInside)
@@ -35,8 +72,8 @@ class ViewController: UIViewController {
     }()
     
     lazy var downButton: UIButton = {
-       let button = UIButton()
-        button.setTitle("Move square down", for: .normal)
+        let button = UIButton()
+        button.setTitle("DOWN", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = .cyan
         button.addTarget(self, action: #selector(animateSquareDown(sender:)), for: .touchUpInside)
@@ -64,19 +101,40 @@ class ViewController: UIViewController {
         addSubviews()
         configureConstraints()
     }
+    @IBAction func changeAnimationTime() {
+        animationTime = Double(timeSlider.value)
+    }
+    
+    @IBAction func animateSquareLeft(sender: UIButton) {
+        let oldOffset = blueSquareCenterXConstraint.constant
+        blueSquareCenterXConstraint.constant = oldOffset - 50
+        UIView.animate(withDuration: self.animationTime) { [unowned self] in
+            self.view.layoutIfNeeded()
+        }
+    }
+    @IBAction func animateSquareRight(sender: UIButton) {
+        let oldOffset = blueSquareCenterXConstraint.constant
+        blueSquareCenterXConstraint.constant = oldOffset + 50
+        UIView.animate(withDuration: self.animationTime) { [unowned self] in
+            self.view.layoutIfNeeded()
+        }
+    }
     
     @IBAction func animateSquareUp(sender: UIButton) {
         let oldOffset = blueSquareCenterYConstraint.constant
-        blueSquareCenterYConstraint.constant = oldOffset - 150
-        UIView.animate(withDuration: 2) { [unowned self] in
+        blueSquareCenterYConstraint.constant = oldOffset - 50
+        
+        UIView.animate(withDuration: self.animationTime) { [unowned self] in
             self.view.layoutIfNeeded()
         }
     }
     
     @IBAction func animateSquareDown(sender: UIButton) {
         let oldOffet = blueSquareCenterYConstraint.constant
-        blueSquareCenterYConstraint.constant = oldOffet + 150
-        UIView.animate(withDuration: 2) { [unowned self] in
+        let timeInterval = TimeInterval(animationTime)
+        dump(timeInterval)
+        blueSquareCenterYConstraint.constant = oldOffet + 50
+        UIView.animate(withDuration: self.animationTime) { [unowned self] in
             self.view.layoutIfNeeded()
         }
     }
@@ -84,29 +142,69 @@ class ViewController: UIViewController {
     private func addSubviews() {
         view.addSubview(blueSquare)
         addStackViewSubviews()
+        addSliderSubviews()
+        addLabelSubviews()
         view.addSubview(buttonStackView)
     }
     
     private func addStackViewSubviews() {
+        buttonStackView.addSubview(leftButton)
+        buttonStackView.addSubview(rightButton)
         buttonStackView.addSubview(upButton)
         buttonStackView.addSubview(downButton)
     }
-    
+    private func addSliderSubviews() {
+        view.addSubview(timeSlider)
+    }
+    private func addLabelSubviews() {
+        view.addSubview(timeLabel)
+    }
     private func configureConstraints() {
         constrainBlueSquare()
         constrainUpButton()
         constrainDownButton()
+        constraintLeftButton()
+        constraintRightButton()
         constrainButtonStackView()
+        constraintSlider()
+        constraintsLabel()
     }
-    
+    private func constraintsLabel() {
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        timeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        timeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        timeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+    }
+    private func constraintSlider() {
+        timeSlider.translatesAutoresizingMaskIntoConstraints = false
+        timeSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        timeSlider.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.80).isActive = true
+        timeSlider.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor, constant: 20).isActive = true
+    }
+    private func constraintLeftButton() {
+        leftButton.translatesAutoresizingMaskIntoConstraints = false
+        leftButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        leftButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        leftButton.leadingAnchor.constraint(equalTo: downButton.trailingAnchor).isActive = true
+    }
+    private func constraintRightButton() {
+        rightButton.translatesAutoresizingMaskIntoConstraints = false
+        rightButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        rightButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        rightButton.trailingAnchor.constraint(equalTo: upButton.leadingAnchor).isActive = true
+        rightButton.leadingAnchor.constraint(equalTo: leftButton.trailingAnchor).isActive = true
+    }
     private func constrainUpButton() {
         upButton.translatesAutoresizingMaskIntoConstraints = false
         upButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        upButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         upButton.trailingAnchor.constraint(equalTo: buttonStackView.trailingAnchor).isActive = true
     }
     
     private func constrainDownButton() {
         downButton.translatesAutoresizingMaskIntoConstraints = false
+        downButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         downButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
